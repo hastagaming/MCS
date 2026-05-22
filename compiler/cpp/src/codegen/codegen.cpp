368796113cc6faf8800d091a2749aea8cfd15f52
hastagaming <rip.hastagaming@gmail.com>
2026-05-22 18:54:36 +0700

    Create codegen/codegen.cpp

---
+++ compiler/cpp/src/codegen/codegen.cpp
@@ -0,0 +1,10 @@
+#include "codegen/codegen.hpp"
+
+namespace macs {
+
+std::string CodeGen::emitLLVMIR(const Program& program) {
+    (void)program;
+    return "; LLVM IR skeleton\n";
+}
+
+} // namespace macs
